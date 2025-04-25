8ee9a4ef8e0bed7a01e7549773acd6e8b5d55f46
nishumital3103 <nishumittal3103@gmail.com>
2025-04-25 14:24:24 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkMode: 'media', // Use system preference by default
+    content: [
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
