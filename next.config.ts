9cc60440d02ae519e5dccc73f7f1c90526fe95b6
nishumital3103 <nishumittal3103@gmail.com>
2025-04-25 15:47:22 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+
+/* @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your configuration options
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add other config properties here
+}
+
+module.exports = nextConfig
