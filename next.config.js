c8fc73a462e27603bd402201aaad90dca5505e11
nishumital3103 <nishumittal3103@gmail.com>
2025-04-25 16:43:03 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+
+/* @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your configuration options
+  reactStrictMode: true,
+  // swcMinify: true,
+  // Add other config properties here
+}
+
+module.exports = nextConfig
